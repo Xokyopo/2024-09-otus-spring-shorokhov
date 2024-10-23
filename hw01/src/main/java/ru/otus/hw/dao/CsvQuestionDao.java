7497d6f984b481a361fb84418c9e918365b591b3
Xokyopo <enslaver@list.ru>
2024-10-23 23:55:19 +0300

    [feat] Реализовал метод "findAll" для класса "CsvQuestionDao"

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,10 +1,16 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -13,11 +19,26 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        return loadQuestionFromCsv().stream()
+                .map(QuestionDto::toDomainObject)
+                .toList();
+    }
 
-        return new ArrayList<>();
+    private List<QuestionDto> loadQuestionFromCsv() throws QuestionReadException {
+        String questionFileName = this.fileNameProvider.getTestFileName();
+        InputStream inputStream = getClass().getResourceAsStream(questionFileName);
+        if (inputStream == null) {
+            throw new QuestionReadException(String.format("Resource %s not fond", questionFileName));
+        }
+
+        try (Reader reader = new InputStreamReader(inputStream)) {
+            return new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(this.fileNameProvider.getTestFileHeaderSize())
+                    .build()
+                    .parse();
+        } catch (IOException e) {
+            throw new QuestionReadException(e.getMessage(), e);
+        }
     }
 }
