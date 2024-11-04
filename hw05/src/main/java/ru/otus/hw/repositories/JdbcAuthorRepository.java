11b0b33a1a25d970cceea8e589e41394df9a36b9
Xokyopo <enslaver@list.ru>
2024-11-04 18:43:22 +0300

    [feat] Добавил поле "jdbcTemplate" в класс "JdbcAuthorRepository" для доступа к данным

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,6 +1,8 @@
 package ru.otus.hw.repositories;
 
+import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
@@ -11,7 +13,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@AllArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public List<Author> findAll() {
