d0e1efd8dda30808b09514952b5b2704129df1b0
Xokyopo <enslaver@list.ru>
2024-11-10 23:04:14 +0300

    [feat] добавил метод "requestStudentAnswer" в класс "TestServiceImpl" запрашвиающий ответы на вопросы

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -20,9 +21,27 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question : questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            var isAnswerValid = requestAnswer(question); // Задать вопрос, получить ответ
             testResult.applyAnswer(question, isAnswerValid);
+            ioService.printLine("");
         }
         return testResult;
+    }
+
+    private boolean requestAnswer(Question question) {
+        int rightAnswer = -1;
+
+        ioService.printLine(question.text());
+        var answers = question.answers();
+        for (int i = 1; i <= answers.size(); i++) {
+            var answer = answers.get(i - 1);
+            ioService.printFormattedLine("\t%s) %s", i, answer.text());
+            if (answer.isCorrect()) {
+                rightAnswer = i;
+            }
+        }
+
+        int studentAnswer = ioService.readIntForRangeWithPrompt(1, answers.size(), ">", "There are no such options");
+        return rightAnswer == studentAnswer;
     }
 }
