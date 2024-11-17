77043bb502a8508e0412ce4ad5151d1e0062f474
Xokyopo <enslaver@list.ru>
2024-11-18 01:02:31 +0300

    [feat] добавил сущность "Comment"

---
+++ hw06/src/main/java/ru/otus/hw/repositories/entities/Comment.java
@@ -0,0 +1,39 @@
+package ru.otus.hw.repositories.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "source_id")
+    private Book source;
+
+    @JoinColumn(name = "text")
+    @Column(length = 1000)
+    private String text;
+}
