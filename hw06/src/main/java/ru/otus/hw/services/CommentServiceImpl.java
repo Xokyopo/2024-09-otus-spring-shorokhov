aa63c2cbda06ad020f70dfdf536ae87b8b533eeb
Xokyopo <enslaver@list.ru>
2024-11-18 01:51:25 +0300

    [feat] добавил реализацию интерфейса "CommentService" как "CommentServiceImpl"

---
+++ hw06/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,61 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+import ru.otus.hw.repositories.entities.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository repository;
+
+    private final BookRepository bookRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Comment> findById(long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Comment> findAllByBookId(long id) {
+        return repository.findAllByBookId(id);
+    }
+
+    @Transactional
+    @Override
+    public Comment insert(String text, long bookId) {
+        var book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(bookId)));
+
+        return repository.save(new Comment(0, book, text));
+    }
+
+    @Transactional
+    @Override
+    public Comment update(long id, String text, long bookId) {
+        var comment = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Comment with id %d not found".formatted(id)));
+        var book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(bookId)));
+
+        comment.setText(text);
+        comment.setSource(book);
+
+        return repository.save(comment);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
+}
