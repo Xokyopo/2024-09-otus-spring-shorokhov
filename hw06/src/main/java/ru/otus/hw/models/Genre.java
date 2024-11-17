e287758fc5eba6c8ff0c95fac386c18ca8a3b84c
Xokyopo <enslaver@list.ru>
2024-11-18 00:01:19 +0300

    [fix] заменил аннотацию lombok "@Data" на ee составляющие для сущности "Genre"

--- hw06/src/main/java/ru/otus/hw/models/Genre.java
+++ hw06/src/main/java/ru/otus/hw/models/Genre.java
@@ -1,12 +1,19 @@
 package ru.otus.hw.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Genre {
     private long id;
 
