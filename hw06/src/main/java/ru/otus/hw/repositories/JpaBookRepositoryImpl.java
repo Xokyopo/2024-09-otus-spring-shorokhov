8a3d40a8ac4a299cfff8dbcaf02066591f1dc8a9
Xokyopo <enslaver@list.ru>
2024-11-21 01:32:44 +0300

    [refactor] удалил избыточный атрибут "ENTITY_GRAPH_TYPE_LOAD" в  "JpaBookRepositoryImpl"

--- hw06/src/main/java/ru/otus/hw/repositories/JpaBookRepositoryImpl.java
+++ hw06/src/main/java/ru/otus/hw/repositories/JpaBookRepositoryImpl.java
@@ -10,10 +10,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
+
 @Repository
 @AllArgsConstructor
 public class JpaBookRepositoryImpl implements BookRepository {
-    private static final String ENTITY_GRAPH_TYPE_LOAD = "jakarta.persistence.loadgraph";
 
     private static final String DEFAULT_ENTITY_GRAPH = "book-main-eg";
 
@@ -23,7 +24,7 @@ public class JpaBookRepositoryImpl implements BookRepository {
     @Override
     public Optional<Book> findById(long id) {
         Object bookEntityGraph = em.getEntityGraph(DEFAULT_ENTITY_GRAPH);
-        Map<String, Object> entityGraphInjection = Map.of(ENTITY_GRAPH_TYPE_LOAD, bookEntityGraph);
+        Map<String, Object> entityGraphInjection = Map.of(EntityGraphType.LOAD.getKey(), bookEntityGraph);
 
         return Optional.ofNullable(em.find(Book.class, id, entityGraphInjection));
     }
@@ -33,7 +34,7 @@ public class JpaBookRepositoryImpl implements BookRepository {
         Object bookEntityGraph = em.getEntityGraph(DEFAULT_ENTITY_GRAPH);
 
         return em.createQuery("FROM Book b", Book.class)
-                .setHint(ENTITY_GRAPH_TYPE_LOAD, bookEntityGraph)
+                .setHint(EntityGraphType.LOAD.getKey(), bookEntityGraph)
                 .getResultList();
     }
 
