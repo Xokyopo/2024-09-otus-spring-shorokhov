136297633bbca3e72f8bd2b92ffb4817ae20136c
Xokyopo <enslaver@list.ru>
2024-11-21 01:41:36 +0300

    [fix] Сделал все связи "LAZY" в сущности "Comment" как того затребовал тест "ModelsCommonTest"

--- hw06/src/main/java/ru/otus/hw/repositories/entities/Comment.java
+++ hw06/src/main/java/ru/otus/hw/repositories/entities/Comment.java
@@ -2,6 +2,7 @@ package ru.otus.hw.repositories.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -53,7 +54,7 @@ public class Comment {
     @JoinColumn(name = "id")
     private long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "source_id")
     private Book source;
 
