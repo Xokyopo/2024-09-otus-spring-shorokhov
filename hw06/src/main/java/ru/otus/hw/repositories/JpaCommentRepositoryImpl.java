1b4d566dce6ba2d0454d4491fa448cb546293f12
Xokyopo <enslaver@list.ru>
2024-11-21 00:23:07 +0300

    [feat] Применил "NamedEntityGraph" при загрузки сущности "Comment" в "JpaCommentRepositoryImpl"

--- hw06/src/main/java/ru/otus/hw/repositories/JpaCommentRepositoryImpl.java
+++ hw06/src/main/java/ru/otus/hw/repositories/JpaCommentRepositoryImpl.java
@@ -1,28 +1,37 @@
 package ru.otus.hw.repositories;
 
+import jakarta.persistence.EntityGraph;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.repositories.entities.Comment;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class JpaCommentRepositoryImpl implements CommentRepository {
+    private static final String ENTITY_GRAPH_TYPE_LOAD = "jakarta.persistence.loadgraph";
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
     public Optional<Comment> findById(long id) {
-        return Optional.ofNullable(em.find(Comment.class, id));
+        EntityGraph<Comment> commentEntityGraph = em.createEntityGraph(Comment.class);
+        Map<String, Object> entityGraphInjection = Map.of(ENTITY_GRAPH_TYPE_LOAD, commentEntityGraph);
+
+        return Optional.ofNullable(em.find(Comment.class, id, entityGraphInjection));
     }
 
     @Override
     public List<Comment> findAllByBookId(long bookId) {
+        EntityGraph<Comment> commentEntityGraph = em.createEntityGraph(Comment.class);
+
         return em.createQuery("FROM Comment c WHERE c.source.id = :bookId", Comment.class)
                 .setParameter("bookId", bookId)
+                .setHint(ENTITY_GRAPH_TYPE_LOAD, commentEntityGraph)
                 .getResultList();
     }
 
