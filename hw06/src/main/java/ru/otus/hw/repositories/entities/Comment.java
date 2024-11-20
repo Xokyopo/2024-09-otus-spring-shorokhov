0c5d151b257bd0735e4ae1e44fe507812312b5ae
Xokyopo <enslaver@list.ru>
2024-11-21 00:17:35 +0300

    [feat] добавил "NamedEntityGraph" для сущности "Comment"

--- hw06/src/main/java/ru/otus/hw/repositories/entities/Comment.java
+++ hw06/src/main/java/ru/otus/hw/repositories/entities/Comment.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedSubgraph;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -15,6 +18,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NamedEntityGraph(
+        attributeNodes = {
+                @NamedAttributeNode(value = "source", subgraph = "source-subgraph")
+        },
+        subgraphs = {
+                @NamedSubgraph(
+                        name = "source-subgraph",
+                        attributeNodes = {
+                                @NamedAttributeNode("author"),
+                                @NamedAttributeNode("genre"),
+                        }
+                )
+        }
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
