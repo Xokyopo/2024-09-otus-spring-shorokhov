fd687d218eba0b8e24bb6a758ae812c84595b4b2
Xokyopo <enslaver@list.ru>
2024-11-27 21:34:42 +0300

    [fix] заменил неправильную аннотацию "JoinColumn"  на "Column" в сущности "Genre"

--- hw06/src/main/java/ru/otus/hw/repositories/entities/Genre.java
+++ hw06/src/main/java/ru/otus/hw/repositories/entities/Genre.java
@@ -1,10 +1,10 @@
 package ru.otus.hw.repositories.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -25,9 +25,9 @@ import lombok.ToString;
 public class Genre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JoinColumn(name = "id")
+    @Column(name = "id")
     private long id;
 
-    @JoinColumn(name = "name")
+    @Column(name = "name")
     private String name;
 }
